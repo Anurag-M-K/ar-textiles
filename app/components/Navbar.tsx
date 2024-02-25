0e5f4d4faf20ec634aa5306389c40cdb4c75cf10
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-02-25 15:01:56 +0530

    navbar responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,27 +1,68 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect, useRef } from 'react';
+import { IoMdMenu } from 'react-icons/io';
 
 function Navbar() {
+  const [navItem, setNavItem] = useState<boolean>(false);
+  const menuRef = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setNavItem(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleClick = () => {
+    setNavItem(!navItem);
+  };
+
   return (
     <div>
-       <ul className='flex justify-center gap-x-3 mt-3 items-center'>
-        <li  className='text-black px-2 py-1  rounded-md  border-2  hover:text-red-400 hover:scale-90 cursor-pointer transition duration-500'>
+      <div className=' absolute right-0 top-10  md:hidden'>
+        <IoMdMenu className='' size={22} onClick={handleClick} color='black' />
+      </div>
+      <div className='md:hidden flex justify-center items-center'>
+        <img
+          className='w-24 h-24'
+          src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT59iVJkvRz1QoUC6kn-PznALguiZB0RQbvgpq5W-sBnA&s'
+          alt=''
+        />
+      </div>
+      <ul
+        ref={menuRef}
+        className={`${
+          navItem || window.innerWidth >= 768 ? 'flex-col' : 'hidden md:flex'
+        } md:flex-row flex flex-wrap justify-center gap-x-3 mt-3 md:items-center`}
+      >
+        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 cursor-pointer transition duration-500'>
           Categories
         </li>
-        <li  className='text-black px-2 py-1  rounded-md  border-2  hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
+        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
           Latest Products
         </li>
-        <li className='border-2 rounded-full hover:scale-90 duration-700 p-3'>
-          <img className='w-24 h-24 ' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT59iVJkvRz1QoUC6kn-PznALguiZB0RQbvgpq5W-sBnA&s" alt="" />
+        <li className='border-2 hidden md:flex text-center rounded-full hover:scale-90 duration-700 p-3'>
+          <img
+            className='w-24 h-24'
+            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT59iVJkvRz1QoUC6kn-PznALguiZB0RQbvgpq5W-sBnA&s'
+            alt=''
+          />
         </li>
-        <li  className='text-black px-2 py-1  rounded-md  border-2  hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
+        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
           Latest Products
         </li>
-        <li  className='text-black px-2 py-1  rounded-md  border-2  hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
-          Latest Products
+        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
+          Find our shop
         </li>
-       </ul>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
