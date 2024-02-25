7bc94ab7bb7334a45fe26c6191c6a5c860f7ee0c
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-02-25 21:29:58 +0530

    navbar completed

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
 
   return (
     <div>
-      <div className=' absolute right-0 top-10  md:hidden'>
+      <div className=' absolute top-10 right-5  md:hidden'>
         <IoMdMenu className='' size={22} onClick={handleClick} color='black' />
       </div>
       <div className='md:hidden flex justify-center items-center'>
@@ -29,23 +29,23 @@ function Navbar() {
           navItem || window.innerWidth >= 768 ? 'flex-col' : 'hidden md:flex'
         } md:flex-row flex flex-wrap justify-center gap-x-3 mt-3 md:items-center`}
       >
-        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 cursor-pointer transition duration-500'>
+        <li className='text-black px-2 py-1 text-center rounded-md border-t  border-b md:border-2 hover:text-red-400 hover:scale-90 cursor-pointer transition duration-500'>
           Categories
         </li>
-        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
+        <li className='text-black px-2 py-1 text-center rounded-md  border-b md:border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
           Latest Products
         </li>
-        <li className='border-2 hidden md:flex text-center rounded-full hover:scale-90 duration-700 p-3'>
+        <li className=' border-b md:border-2 hidden md:flex text-center rounded-full hover:scale-90 duration-700 p-3'>
           <img
             className='w-24 h-24'
             src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT59iVJkvRz1QoUC6kn-PznALguiZB0RQbvgpq5W-sBnA&s'
             alt=''
           />
         </li>
-        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
+        <li className='text-black px-2 py-1 text-center rounded-md  border-b md:border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
           Latest Products
         </li>
-        <li className='text-black px-2 py-1 text-center rounded-md border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
+        <li className='text-black px-2 py-1 text-center rounded-md  border-b md:border-2 hover:text-red-400 hover:scale-90 transition duration-500 cursor-pointer'>
           Find our shop
         </li>
       </ul>
