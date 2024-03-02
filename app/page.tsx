3b329ecdfe47a9e4f7d5fbb66766291278e2af37
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-03-02 09:10:19 +0530

    carausal added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Carausal from "./components/Carausal";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
+      <Carausal/>
     </main>
   );
 }
