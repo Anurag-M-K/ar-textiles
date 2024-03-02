680f3028446375d0683e1c8ad8de09b8baf1381b
Anurag M K <109131413+Anurag-M-K@users.noreply.github.com>
2024-03-02 09:51:05 +0530

    category section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Carausal from "./components/Carausal";
+import Category from "./components/Category";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar/>
       <Carausal/>
+      <Category/>
     </main>
   );
 }
